b516f9ff6cd176c574674090fffd02323bc093b0
SProgrammer13 <Aleksandrafomina2022@gmail.com>
2025-02-04 22:22:48 +0200

    created the timer and the start screen

--- js/script.js
+++ js/script.js
@@ -1,12 +1,35 @@
 const questionEl = document.querySelector('.question');
+const startScreen = document.querySelector('.start-screen');
 const mainScreen = document.querySelector('.main-screen');
 const endScreen = document.querySelector('.end-screen');
 const answerList = document.querySelectorAll('.answer');
+const startBtn = document.querySelector('.start-btn');
 const skipBtn = document.querySelector('.skip-btn');
+const gameTimerEl = document.querySelector('.game-timer');
+const warning = document.querySelector('.warning');
 let currentQuestion;
 let questions;
 let currentIndex = 0;
 let points = 0;
+let timer = 10;
+
+startBtn.addEventListener('click', startQuiz);
+
+function startQuiz(){
+        gameTimerEl.textContent = timer;
+        mainScreen.classList.remove('hide');
+        startScreen.classList.add('hide');  
+        setInterval(startTimer, 1000);
+}
+
+function startTimer(){
+        timer--;
+        gameTimerEl.textContent = timer;
+        if(timer < 0){
+            endQuiz();
+            gameTimerEl.textContent = 0;
+        }
+}
 
 async function getQuestions(){
     let response = await fetch('question-list.json');
